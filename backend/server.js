4547413024c3ea20ac365e9b4358dc1e6c23f30f
Kumiho <pavleta.taseva@gmail.com>
2022-02-24 15:29:22 +0200

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware.js');
+const connectDB = require('./config/db.js');
+
+connectDB();
 
 const app = express();
 
